6c956dbd1741026435e9718e624b66ae655f2c80
Suresh <madhanamonisuresh9@gmail.com>
2025-10-12 11:12:24 +0530

    Odd Even placing

---
+++ LinkedList/SLL/odd_even.py
@@ -0,0 +1,51 @@
+#Optimal  
+#n = 6 
+#input = 1,2,3,4,5,6
+#output = 1,3,5,2,4,6
+class Node:
+    def __init__(self, value, next_node=None):
+        self.data = value
+        self.next = next_node
+
+def array_to_LL(arr):
+    if not arr:
+        return None
+    head = Node(arr[0])
+    cur = head
+    for i in range(1, len(arr)):
+        temp = Node(arr[i])
+        cur.next = temp
+        cur = cur.next
+    return head
+
+def print_LL(head):
+    temp = head
+    while temp:
+        print(temp.data, end=" ")
+        temp = temp.next
+
+
+def even_odd_list(head):
+    even = head
+    odd = head.next
+    odd_head = head.next
+
+    while odd and odd.next:
+        even.next = even.next.next
+        odd.next = odd.next.next
+        even = even.next
+        odd = odd.next
+
+    even.next = odd_head
+    return head
+
+
+n = int(input("Enter Num: "))
+arr = list(map(int, input("Enter the Arr: ").split()))
+head = array_to_LL(arr)
+head = even_odd_list(head)
+print_LL(head)
+
+
+#Brute Force Solution  
+
